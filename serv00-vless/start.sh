20f3175523832587da5101268a9731c502d1ad59
lopins <lopins@issem.cn>
2024-11-05 19:15:02 +0800

    :recycle:refactor: CommonJS to ES

--- serv00-vless/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-export NEZHA_SERVER="abc.xyz:5555"
-export NEZHA_KEY="abcdwfghijklmnopqrstuvwxyz"
-
-chmod +x swith start.sh
-nohup ./swith -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &   #需要tls在 > 前面加上 --tls即可
-
-tail -f /dev/null
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-const fs = require('fs');
-const puppeteer = require('puppeteer');
-const axios = require('axios');
+import fs from 'fs';
+import path from 'path';
+import puppeteer from 'puppeteer';
+import axios from 'axios';
+import { fileURLToPath } from 'url';
 
 function formatToISO(date) {
     return date.toISOString().replace('T', ' ').replace('Z', '').replace(/\.\d{3}Z/, '');
@@ -14,38 +16,52 @@ async function sendTelegramMessage(token, chatId, message) {
     const url = `https://api.telegram.org/bot${token}/sendMessage`;
     const data = {
         chat_id: chatId,
-        text: message,
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: '问题反馈❓',
-                        url: 'https://t.me/lopinv'
-                    }
-                ]
-            ]
-        }
+        text: message
     };
-
     try {
         const response = await axios.post(url, data);
-        console.log('消息已发送到 Telegram:', response.data);
+        // console.log('消息已发送到 Telegram:', response.data);
+        console.log('消息已发送到 Telegram');
     } catch (error) {
-        console.error('发送 Telegram 消息时出错:', error.response ? error.response.data : error.message);
+        // if (error.response) {
+        //     console.error('发送 Telegram 消息时出错:', error.response.status, error.response.data);
+        // } else if (error.request) {
+        //     console.error('发送 Telegram 消息时出错:', error.request);
+        // } else {
+        //     console.error('发送 Telegram 消息时出错:', error.message);
+        // }
+        console.error('Telegram 消息发生失败');
     }
 }
 
 (async () => {
-    const accountsJson = fs.readFileSync('accounts.json', 'utf-8');
-    const accounts = JSON.parse(accountsJson);
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const accounts = JSON.parse(fs.readFileSync(path.join(__dirname, '../accounts.json'), 'utf-8'));
     const telegramToken = process.env.TELEGRAM_BOT_TOKEN;
     const telegramChatId = process.env.TELEGRAM_CHAT_ID;
 
     for (const account of accounts) {
         const { username, password, panel } = account;
 
-        const browser = await puppeteer.launch({ headless: true });
+        // 显示浏览器窗口&使用自定义窗口大小
+        const browser = await puppeteer.launch({ 
+            headless: false, 
+            // args: [
+            //     '--no-sandbox',
+            //     '--disable-setuid-sandbox',
+            //     '--disable-dev-shm-usage',
+            //     '--disable-infobars',
+            //     '--disable-blink-features=AutomationControlled'
+            // ],
+            // defaultViewport: null,
+            // ignoreHTTPSErrors: true
+        });
         const page = await browser.newPage();
+        // await page.setViewport({ width: 1366, height: 768 });
+        // await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36');
+        // await page.evaluateOnNewDocument(() => {
+        //     delete Object.getPrototypeOf(navigator).webdriver;
+        // });
 
         let url = `https://${panel}/login/?next=/`;
 
@@ -93,10 +109,14 @@ async function sendTelegramMessage(token, chatId, message) {
                 await sendTelegramMessage(telegramToken, telegramChatId, `账号 ${username} 登录时出现错误: ${error.message}`);
             }
         } finally {
+            // 模拟人类行为
+            // await page.waitForTimeout(1000 + Math.floor(Math.random() * 2000)); 
+            // await page.type('#id_username', 'testuser', { delay: 100 + Math.floor(Math.random() * 100) });
+            // await page.click('#submit');
+            // await page.waitForNavigation();
             await page.close();
             await browser.close();
-
-            const delay = Math.floor(Math.random() * 8000) + 1000; // 随机延时1秒到8秒之间
+            const delay = Math.floor(Math.random() * 5000) + 1000; // 随机延时1秒到5秒之间
             await delayTime(delay);
         }
     }
